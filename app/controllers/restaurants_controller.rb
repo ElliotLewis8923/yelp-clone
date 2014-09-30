c371f6ff842e250ce6389a0d4c8b70b3e934d1a3
elliotlewis8923 <elewis8923@gmail.com>
2014-09-30 11:58:39 +0100

    restaurants can now be edited by the user

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,7 +10,7 @@ class RestaurantsController < ApplicationController
 	end
 
 	def create
-		@restaurant = Restaurant.new(article_params)
+		@restaurant = Restaurant.new(restaurant_params)
 		@restaurant.save
 		redirect_to '/restaurants'
 	end
@@ -19,8 +19,23 @@ class RestaurantsController < ApplicationController
 		@restaurant = Restaurant.find(params[:id])
 	end
 
+	def edit
+		@restaurant = Restaurant.find(params[:id])
+	end
+
+	def update
+		@restaurant = Restaurant.find(params[:id])
+
+
+		if @restaurant.update(restaurant_params)
+			redirect_to @restaurant
+		else
+			render 'edit'
+		end
+	end
+
 	private
-		def article_params
+		def restaurant_params
 			params.require(:restaurant).permit(:name, :cuisine)
 		end
 
--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -9,7 +9,7 @@ describe 'Users' do
 			click_link 'Add a new restaurant'
 			fill_in 'name', :with => "The Nag's Head"
 			fill_in 'cuisine', :with => "Slime"
-			click_button 'submit'
+			click_button 'Submit new restaurant'
 			expect(page).to have_content "The Nag's Head"
 			expect(Restaurant.count).to eq 1
 		end
@@ -21,6 +21,20 @@ describe 'Users' do
 			click_link 'Benets'
 			expect(page).to have_content 'Tasty snax'
 		end
+
+		it 'can edit an existing restaurant' do
+			Restaurant.create(name: 'Artisam',
+												cuisine: 'Titilating grub from the Orient')
+			visit '/restaurants'
+			click_link 'Artisam'
+			click_link 'Edit'
+			fill_in 'name', :with => 'peng'
+			fill_in 'cuisine', :with => 'yo'
+			click_button 'Submit changes'
+			expect(Restaurant.find_or_create_by(name: 'peng').cuisine).to eq 'yo'
+		end
+
+
 
 	end
 
